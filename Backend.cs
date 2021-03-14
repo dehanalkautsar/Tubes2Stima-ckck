f73fc3d1d39f7ebde5faaca5772563467d20fc0f
azharfatrr <m.azhar.faturahman@gmail.com>
2021-03-14 21:01:35 +0700

    hehe

--- Backend.cs
+++ Backend.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Tubes2Stima_ckck
 {
     class Backend
     {
-        
     }
 }
--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -4,12 +4,12 @@ namespace Tubes2Stima_ckck
     partial class Form1
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -24,64 +24,38 @@ namespace Tubes2Stima_ckck
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.Test1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
-            // 
-            // Test1
-            // 
-            this.Test1.Location = new System.Drawing.Point(55, 45);
-            this.Test1.Name = "Test1";
-            this.Test1.Size = new System.Drawing.Size(124, 24);
-            this.Test1.TabIndex = 0;
-            this.Test1.Text = "Test1";
-            this.Test1.Click += new System.EventHandler(this.label1_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(55, 102);
+            this.button1.Location = new System.Drawing.Point(118, 71);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(181, 29);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Cari jodohnya";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(35, 207);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(432, 95);
-            this.textBox1.TabIndex = 2;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(550, 683);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(633, 548);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.Test1);
             this.Name = "Form1";
-            this.Text = "Dehan Mencari Teman";
+            this.Text = "Form1";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label Test1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
