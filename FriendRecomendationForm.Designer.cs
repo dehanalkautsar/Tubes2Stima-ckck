2236f16b770de5783847d52add9b9d03b7cf52c5
auliaadila <auliaadila36@gmail.com>
2021-03-23 20:45:46 +0700

    gui friend rec

--- FriendRecomendationForm.Designer.cs
+++ FriendRecomendationForm.Designer.cs
@@ -32,10 +32,6 @@ namespace Tubes2Stima_ckck
             this.labelFriendRecomendation = new System.Windows.Forms.Label();
             this.titleExplore = new System.Windows.Forms.Label();
             this.pathExplore = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // labelFriendRecomendation
@@ -68,64 +64,16 @@ namespace Tubes2Stima_ckck
             this.pathExplore.TabIndex = 21;
             this.pathExplore.Text = "SiapaKeSiapa";
             // 
-            // label3
-            // 
-            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(20, 105);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(196, 19);
-            this.label3.TabIndex = 22;
-            this.label3.Text = "Node1";
-            // 
-            // label5
-            // 
-            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label5.Font = new System.Drawing.Font("Comic Sans MS", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(20, 124);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(365, 31);
-            this.label5.TabIndex = 24;
-            this.label5.Text = "List Mutual Friend";
-            // 
-            // label4
-            // 
-            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label4.Font = new System.Drawing.Font("Comic Sans MS", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(20, 174);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(365, 31);
-            this.label4.TabIndex = 26;
-            this.label4.Text = "List Mutual Friend";
-            // 
-            // label6
-            // 
-            this.label6.Font = new System.Drawing.Font("Comic Sans MS", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(20, 155);
-            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(196, 19);
-            this.label6.TabIndex = 25;
-            this.label6.Text = "Node2";
-            // 
             // FriendRecomendationForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
             this.ClientSize = new System.Drawing.Size(400, 522);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.pathExplore);
             this.Controls.Add(this.titleExplore);
             this.Controls.Add(this.labelFriendRecomendation);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.MinimumSize = new System.Drawing.Size(416, 519);
             this.Name = "FriendRecomendationForm";
             this.Text = "IniTeman";
@@ -137,9 +85,5 @@ namespace Tubes2Stima_ckck
         private System.Windows.Forms.Label labelFriendRecomendation;
         private System.Windows.Forms.Label titleExplore;
         private System.Windows.Forms.Label pathExplore;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label6;
     }
 }
--- FriendRecomendationForm.cs
+++ FriendRecomendationForm.cs
@@ -26,12 +26,12 @@ namespace Tubes2Stima_ckck
             InitializeComponent();
             titleExploreFriend();
             pathExploreEditor();
-            AddComponent();
+            AddComponentFriendR();
         }
 
         public void titleExploreFriend()
         {
-            string text = "Explore Friends Between " + this.initialNode + " - " + this.targetNode;
+            string text = "Explore Friends Between: " + this.initialNode + " - " + this.targetNode;
             this.titleExplore.Text = text;
         }
 
@@ -72,43 +72,60 @@ namespace Tubes2Stima_ckck
             this.pathExplore.Text = text;
         }
 
-        public void AddComponent()
+        public void AddComponentFriendR()
         {
+            this.labelFriendRecomendation.Text = "Friend Recommendations for: " + initialNode;
             // EDIT DISINI
-            int i = 2;
+            int i = 0;
             this.SuspendLayout();
-
+            Dictionary<string, List<string>> dict = openedGraph.allMutual(initialNode);
+            //Dictionary<string, List<string>> allMutual(string username1)
+            int offset = 0;
             // Ganti loop ini jadi apa yang mau dibuat
-            while (i < 8)
-            { 
+            foreach (KeyValuePair<string, List<string>> friend in dict.OrderByDescending(key => key.Value.Count()))
+            {
+                
+                //Console.WriteLine("Key: {0}, Value: {1}", author.Key, author.Value.Count());
                 // Label nodeLabel 
                 Label nodeLabel = new Label();
                 // Edit properties nodeLabel
                 nodeLabel.Font = new System.Drawing.Font("Comic Sans MS", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-                nodeLabel.Location = new System.Drawing.Point(20, 120 + i * 55);
+                nodeLabel.Location = new System.Drawing.Point(15, 110 + i * 55 + offset);
                 nodeLabel.Name = "labelNode" + i.ToString();
-                //nodeLabel.Size = new System.Drawing.Size(262, 23);
-                nodeLabel.Text = "Node" + i.ToString();
+                nodeLabel.Size = new System.Drawing.Size(262, 15);
+                nodeLabel.Text = "Nama akun: " + friend.Key.ToString();
                 // Tambah ke form
                 this.Controls.Add(nodeLabel);
 
                 // Label listMutualFriend
                 Label listMutualFriend = new Label();
+                int countMutual = friend.Value.Count();
                 // Edit properties listMutualFriend
                 listMutualFriend.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
                 listMutualFriend.Font = new System.Drawing.Font("Comic Sans MS", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-                listMutualFriend.Location = new System.Drawing.Point(20, 140 + i * 55);
+                listMutualFriend.Location = new System.Drawing.Point(20, 128 + i * 55 + offset);
                 listMutualFriend.Name = "labelListMutualFriend" + i.ToString();
-                //listMutualFriend.Size = new System.Drawing.Size(487, 38);
-                listMutualFriend.Text = "List Mutual Friend" + i.ToString();
+                listMutualFriend.Size = new System.Drawing.Size(487, 15 + countMutual * 15);
+                listMutualFriend.Text = friend.Value.Count().ToString() + " Mutual Friend:\n";
+                
+                foreach(var item in friend.Value)
+                {
+                    listMutualFriend.Text = listMutualFriend.Text + item.ToString() + "\n";
+                    offset += 15;
+                }
+                
+
                 // Tambah ke form
                 this.Controls.Add(listMutualFriend);
 
                 i++;
+
             }
+
             this.ResumeLayout(false);
 
         }
 
+    
     }
 }
